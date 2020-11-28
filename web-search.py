c434c1997fa8c28ad2cf510ec1c9d220a099f8a9
Coder <72826534+PythonCoder8@users.noreply.github.com>
2020-11-27 17:03:55 -0800

    Update web-search.py

--- web-search.py
+++ web-search.py
@@ -7,6 +7,8 @@
 
 #########################################################################################
 
+#Import required libraries
+
 from time import time
 from googlesearch import search
 import sys
@@ -15,11 +17,13 @@ import webbrowser
 search_query = input('What do you want to search for on the web?: ')
 result_num = input('How many results do you want to retrieve from the web?: ')
 
+#Verify user input
 try:
     int_result_num = int(result_num)
 except:
     sys.exit('The number of results you wanted to retrieve was not a number. Exiting program...')
 
+#Display web results
 print('The top %d results from the web are:' %(int_result_num))
 
 start = time()
@@ -28,6 +32,8 @@ for url in search(search_query, tld='com', stop=int_result_num):
 end = time()
 
 print('Found %d results from the web in %s' %(int_result_num, end - start) + ' seconds.')
+
+#Ask user if they want to open the links in the web browser with validation
 open_in_browser = input('Do you want to open the given URLs in your web browser (Y/N)? ')
 
 if open_in_browser.upper() == 'Y':
