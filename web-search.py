84f530d03ef23654c6a69597b08ec586dd3ccc0c
Coder <72826534+PythonCoder8@users.noreply.github.com>
2020-11-28 17:48:20 -0800

    Update web-search.py

--- web-search.py
+++ web-search.py
@@ -7,61 +7,63 @@
 
 #########################################################################################
 
-
-from time import time
-from googlesearch import search
-import sys
-import webbrowser
-
-#Print title using variables and delete after using variables to clear up RAM
-width = 40
-titletxt = 'Web-Search'
-title = titletxt.center(width)
-print(title)
-del width
-del titletxt
-del title
-
-search_query = input('\nWhat do you want to search for on the web?: ')
-result_num = input('How many results do you want to retrieve from the web?: ')
-
-#Verify user input
 try:
-    int_result_num = int(result_num)
-except:
-    sys.exit('The number of results you wanted to retrieve was not a number. Exiting program...')
+    from time import time
+    from googlesearch import search
+    import sys
+    import webbrowser
 
-#Display web results
-print('The top %d results from the web are:' %(int_result_num))
+    #Print title using variables and delete after using variables to clear up RAM
+    width = 40
+    titletxt = 'Web-Search'
+    title = titletxt.center(width)
+    print(title)
+    del width
+    del titletxt
+    del title
 
-start = time()
-for url in search(search_query, tld='com', stop=int_result_num):
-    print(url + " - %s" %(url.split('/')[2]))
-end = time()
+    search_query = input('\nWhat do you want to search for on the web?: ')
+    result_num = input('How many results do you want to retrieve from the web?: ')
 
-print('Found %d results from the web in %s' %(int_result_num, end - start) + ' seconds.')
+    #Verify user input
+    try:
+        int_result_num = int(result_num)
+    except:
+        sys.exit('The number of results you wanted to retrieve was not a number. Exiting program...')
 
-#Delete time variables to clear RAM
-del start
-del end
+    #Display web results
+    print('The top %d results from the web are:' %(int_result_num))
 
-#Ask user if they want to open the links in the web browser with validation
-open_in_browser = input('Do you want to open the given URLs in your web browser (Y/N)? ')
-
-if open_in_browser.upper() == 'Y':
+    start = time()
     for url in search(search_query, tld='com', stop=int_result_num):
-        webbrowser.open(url)
+        print(url + " - %s" %(url.split('/')[2]))
+    end = time()
 
-elif open_in_browser.upper() == 'N':
-    sys.exit('Ok! Bye!')
+    print('Found %d results from the web in %s' %(int_result_num, end - start) + ' seconds.')
 
-else:
-    while open_in_browser.upper() != 'Y' and open_in_browser.upper() != 'N':
-        open_in_browser = input('Invalid response! Do you want to open the given URLs in your web browser (Y/N)? ')
+    #Delete time variables to clear RAM
+    del start
+    del end
 
-        if open_in_browser.upper() == 'Y':
-            for url in search(search_query, tld='com', stop=int_result_num):
-                webbrowser.open(url)
+    #Ask user if they want to open the links in the web browser with validation
+    open_in_browser = input('Do you want to open the given URLs in your web browser (Y/N)? ')
 
-        elif open_in_browser.upper() == 'N':
-            sys.exit('Ok! Bye!')
+    if open_in_browser.upper() == 'Y':
+        for url in search(search_query, tld='com', stop=int_result_num):
+            webbrowser.open(url)
+
+    elif open_in_browser.upper() == 'N':
+        sys.exit('Ok! Bye!')
+
+    else:
+        while open_in_browser.upper() != 'Y' and open_in_browser.upper() != 'N':
+            open_in_browser = input('Invalid response! Do you want to open the given URLs in your web browser (Y/N)? ')
+
+            if open_in_browser.upper() == 'Y':
+                for url in search(search_query, tld='com', stop=int_result_num):
+                    webbrowser.open(url)
+
+            elif open_in_browser.upper() == 'N':
+                sys.exit('Ok! Bye!')
+except:
+    print("\033[1;31;40mSomething went wrong.")
